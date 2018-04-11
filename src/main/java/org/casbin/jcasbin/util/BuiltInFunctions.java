799c9df382084093faeae12a4f61b25d45423dec
Yang Luo <hsluoyz@qq.com>
2018-04-11 13:12:30 +0800

    Add built-in functions.

--- src/main/java/org/casbin/jcasbin/util/BuiltInFunctions.java
+++ src/main/java/org/casbin/jcasbin/util/BuiltInFunctions.java
@@ -14,27 +14,75 @@
 
 package org.casbin.jcasbin.util;
 
+import java.util.regex.Pattern;
+
 public class BuiltInFunctions {
     /**
      * keyMatch determines whether key1 matches the pattern of key2 (similar to RESTful path), key2 can contain a *.
      * For example, "/foo/bar" matches "/foo/*"
      */
     public static boolean keyMatch(String key1, String key2) {
-        return true;
+        int i = key2.indexOf('*');
+        if (i == -1) {
+            return key1.equals(key2);
+        }
+
+        if (key1.length() > i) {
+            return key1.substring(0, i).equals(key2.substring(0, i));
+        }
+        return key1.equals(key2.substring(0, i));
+    }
+
+    /**
+     * keyMatch2 determines whether key1 matches the pattern of key2 (similar to RESTful path), key2 can contain a *.
+     * For example, "/foo/bar" matches "/foo/*", "/resource1" matches "/:resource"
+     */
+    public static boolean keyMatch2(String key1, String key2) {
+        key2 = key2.replace("/*", "/.*");
+
+        Pattern p = Pattern.compile("(.*):[^/]+(.*)");
+        while (true) {
+            if (!key2.contains("/:")) {
+                break;
+            }
+
+            key2 = "^" + p.matcher(key2).replaceAll("$1[^/]+$2") + "$";
+        }
+
+        return regexMatch(key1, key2);
+    }
+
+    /**
+     * keyMatch3 determines whether key1 matches the pattern of key2 (similar to RESTful path), key2 can contain a *.
+     * For example, "/foo/bar" matches "/foo/*", "/resource1" matches "/{resource}"
+     */
+    public static boolean keyMatch3(String key1, String key2) {
+        key2 = key2.replace("/*", "/.*");
+
+        Pattern p = Pattern.compile("(.*)\\{[^/]+\\}(.*)");
+        while (true) {
+            if (!key2.contains("/{")) {
+                break;
+            }
+
+            key2 = p.matcher(key2).replaceAll("$1[^/]+$2");
+        }
+
+        return regexMatch(key1, key2);
     }
 
     /**
      * regexMatch determines whether key1 matches the pattern of key2 in regular expression.
      */
     public static boolean regexMatch(String key1, String key2) {
-        return true;
+        return Pattern.matches(key2, key1);
     }
 
     /**
      * ipMatch determines whether IP address ip1 matches the pattern of IP address ip2, ip2 can be an IP address or a CIDR pattern.
      * For example, "192.168.2.123" matches "192.168.2.0/24"
      */
-    public static boolean ipMatch(String key1, String key2) {
+    public static boolean ipMatch(String ip1, String ip2) {
         return true;
     }
 }
---
+++ src/main/java/org/casbin/jcasbin/util/function/KeyMatch2Func.java
@@ -0,0 +1,39 @@
+// Copyright 2018 The casbin Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.casbin.jcasbin.util.function;
+
+import com.googlecode.aviator.runtime.function.AbstractFunction;
+import com.googlecode.aviator.runtime.function.FunctionUtils;
+import com.googlecode.aviator.runtime.type.AviatorBoolean;
+import com.googlecode.aviator.runtime.type.AviatorObject;
+import org.casbin.jcasbin.util.BuiltInFunctions;
+
+import java.util.Map;
+
+/**
+ * KeyMatch2Func is the wrapper for keyMatch2.
+ */
+public class KeyMatch2Func extends AbstractFunction {
+    public AviatorObject call(Map<String, Object> env, AviatorObject arg1, AviatorObject arg2) {
+        String key1 = FunctionUtils.getStringValue(arg1, env);
+        String key2 = FunctionUtils.getStringValue(arg2, env);
+
+        return AviatorBoolean.valueOf(BuiltInFunctions.keyMatch2(key1, key2));
+    }
+
+    public String getName() {
+        return "keyMatch2";
+    }
+}

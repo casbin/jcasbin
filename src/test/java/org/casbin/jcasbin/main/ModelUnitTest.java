13428acff81ee8b118ad0296a0b1e7167c154525
Yang Luo <hsluoyz@qq.com>
2018-04-14 17:43:43 +0800

    Add some tests to ModelUnitTest.

---
+++ src/test/java/org/casbin/jcasbin/main/ModelUnitTest.java
@@ -0,0 +1,127 @@
+// Copyright 2018 The casbin Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.casbin.jcasbin.main;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ModelUnitTest {
+    public void testEnforce(Enforcer e, String sub, String obj, String act, boolean res) {
+        assertEquals(e.enforce(sub, obj, act), res);
+    }
+
+    public void testEnforceWithoutUsers(Enforcer e, String obj, String act, boolean res) {
+        assertEquals(e.enforce(obj, act), res);
+    }
+
+    @Test
+    public void testBasicModel() {
+        Enforcer e = new Enforcer("examples/basic_model.conf", "examples/basic_policy.csv");
+
+        testEnforce(e, "alice", "data1", "read", true);
+        testEnforce(e, "alice", "data1", "write", false);
+        testEnforce(e, "alice", "data2", "read", false);
+        testEnforce(e, "alice", "data2", "write", false);
+        testEnforce(e, "bob", "data1", "read", false);
+        testEnforce(e, "bob", "data1", "write", false);
+        testEnforce(e, "bob", "data2", "read", false);
+        testEnforce(e, "bob", "data2", "write", true);
+    }
+
+    @Test
+    public void testBasicModelNoPolicy() {
+        Enforcer e = new Enforcer("examples/basic_model.conf");
+
+        testEnforce(e, "alice", "data1", "read", false);
+        testEnforce(e, "alice", "data1", "write", false);
+        testEnforce(e, "alice", "data2", "read", false);
+        testEnforce(e, "alice", "data2", "write", false);
+        testEnforce(e, "bob", "data1", "read", false);
+        testEnforce(e, "bob", "data1", "write", false);
+        testEnforce(e, "bob", "data2", "read", false);
+        testEnforce(e, "bob", "data2", "write", false);
+    }
+
+    @Test
+    public void testBasicModelWithRoot() {
+        Enforcer e = new Enforcer("examples/basic_with_root_model.conf", "examples/basic_policy.csv");
+
+        testEnforce(e, "alice", "data1", "read", true);
+        testEnforce(e, "alice", "data1", "write", false);
+        testEnforce(e, "alice", "data2", "read", false);
+        testEnforce(e, "alice", "data2", "write", false);
+        testEnforce(e, "bob", "data1", "read", false);
+        testEnforce(e, "bob", "data1", "write", false);
+        testEnforce(e, "bob", "data2", "read", false);
+        testEnforce(e, "bob", "data2", "write", true);
+        testEnforce(e, "root", "data1", "read", true);
+        testEnforce(e, "root", "data1", "write", true);
+        testEnforce(e, "root", "data2", "read", true);
+        testEnforce(e, "root", "data2", "write", true);
+    }
+
+    @Test
+    public void testBasicModelWithRootNoPolicy() {
+        Enforcer e = new Enforcer("examples/basic_with_root_model.conf");
+
+        testEnforce(e, "alice", "data1", "read", false);
+        testEnforce(e, "alice", "data1", "write", false);
+        testEnforce(e, "alice", "data2", "read", false);
+        testEnforce(e, "alice", "data2", "write", false);
+        testEnforce(e, "bob", "data1", "read", false);
+        testEnforce(e, "bob", "data1", "write", false);
+        testEnforce(e, "bob", "data2", "read", false);
+        testEnforce(e, "bob", "data2", "write", false);
+        testEnforce(e, "root", "data1", "read", true);
+        testEnforce(e, "root", "data1", "write", true);
+        testEnforce(e, "root", "data2", "read", true);
+        testEnforce(e, "root", "data2", "write", true);
+    }
+
+    @Test
+    public void testBasicModelWithoutUsers() {
+        Enforcer e = new Enforcer("examples/basic_without_users_model.conf", "examples/basic_without_users_policy.csv");
+
+        testEnforceWithoutUsers(e, "data1", "read", true);
+        testEnforceWithoutUsers(e, "data1", "write", false);
+        testEnforceWithoutUsers(e, "data2", "read", false);
+        testEnforceWithoutUsers(e, "data2", "write", true);
+    }
+
+    @Test
+    public void testBasicModelWithoutResources() {
+        Enforcer e = new Enforcer("examples/basic_without_resources_model.conf", "examples/basic_without_resources_policy.csv");
+
+        testEnforceWithoutUsers(e, "alice", "read", true);
+        testEnforceWithoutUsers(e, "alice", "write", false);
+        testEnforceWithoutUsers(e, "bob", "read", false);
+        testEnforceWithoutUsers(e, "bob", "write", true);
+    }
+
+    @Test
+    public void testRBACModel() {
+        Enforcer e = new Enforcer("examples/rbac_model.conf", "examples/rbac_policy.csv");
+
+        testEnforce(e, "alice", "data1", "read", true);
+        testEnforce(e, "alice", "data1", "write", false);
+        testEnforce(e, "alice", "data2", "read", true);
+        testEnforce(e, "alice", "data2", "write", true);
+        testEnforce(e, "bob", "data1", "read", false);
+        testEnforce(e, "bob", "data1", "write", false);
+        testEnforce(e, "bob", "data2", "read", false);
+        testEnforce(e, "bob", "data2", "write", true);
+    }
+}

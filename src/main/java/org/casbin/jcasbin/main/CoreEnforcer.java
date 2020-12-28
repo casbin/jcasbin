6f61945ad390b87d9b6fd154f7d9d4f0c014a065
shy <928357057@qq.com>
2020-12-28 09:59:21 +0800

    add Dispatcher.
    Signed-off-by: shy <928357057@qq.com>

--- src/main/java/org/casbin/jcasbin/main/CoreEnforcer.java
+++ src/main/java/org/casbin/jcasbin/main/CoreEnforcer.java
@@ -27,6 +27,7 @@ import org.casbin.jcasbin.model.Assertion;
 import org.casbin.jcasbin.model.FunctionMap;
 import org.casbin.jcasbin.model.Model;
 import org.casbin.jcasbin.persist.Adapter;
+import org.casbin.jcasbin.persist.Dispatcher;
 import org.casbin.jcasbin.persist.WatcherEx;
 import org.casbin.jcasbin.persist.file_adapter.FilteredAdapter;
 import org.casbin.jcasbin.persist.Watcher;
@@ -51,11 +52,13 @@ public class CoreEnforcer {
     Adapter adapter;
     Watcher watcher;
     RoleManager rm;
+    Dispatcher dispatcher;
 
     private boolean enabled;
     boolean autoSave;
     boolean autoBuildRoleLinks;
     boolean autoNotifyWatcher = true;
+    boolean autoNotifyDispatcher = true;
 
     // cached instance of AviatorEvaluatorInstance
     AviatorEvaluatorInstance aviatorEval;
@@ -71,6 +74,7 @@ public class CoreEnforcer {
         enabled = true;
         autoSave = true;
         autoBuildRoleLinks = true;
+        dispatcher = null;
     }
 
     /**
@@ -176,6 +180,15 @@ public class CoreEnforcer {
     public void setWatcher(Watcher watcher) {
         this.watcher = watcher;
         watcher.setUpdateCallback(this::loadPolicy);
+    }
+
+    /**
+     * setDispatcher sets the current dispatcher.
+     *
+     * @param dispatcher jCasbin dispatcher
+     */
+    public void setDispatcher(Dispatcher dispatcher) {
+        this.dispatcher = dispatcher;
     }
 
     /**
@@ -506,5 +519,13 @@ public class CoreEnforcer {
 
     public void setAutoNotifyWatcher(boolean autoNotifyWatcher) {
         this.autoNotifyWatcher = autoNotifyWatcher;
+    }
+
+    public boolean isAutoNotifyDispatcher() {
+        return autoNotifyDispatcher;
+    }
+
+    public void setAutoNotifyDispatcher(boolean autoNotifyDispatcher) {
+        this.autoNotifyDispatcher = autoNotifyDispatcher;
     }
 }
